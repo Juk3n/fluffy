24b43d1d28a2b03339c67477525b3857c6c58bb4
juk3n <adekenn3@gmail.com>
2024-07-02 13:53:31 +0200

    removed sample test

--- app/tests/gameTest.cpp
+++ app/tests/gameTest.cpp
@@ -2,12 +2,10 @@
 
 #include <game.hpp>
 
-// Demonstrate some basic assertions.
 TEST(GameTest, Creation) {
   Game sut{0, "sut", "sut/path"};
 
   EXPECT_EQ(0, sut.getId());
   EXPECT_EQ("sut", sut.getName());
   EXPECT_EQ("sut/path", sut.getPath());
-
 }
