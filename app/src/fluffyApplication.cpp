cc9eb4559e80dbffb8ca7422ff2fbd73cfb57301
Adrian Duraj <adrian.duraj@onmail.com>
2024-11-26 17:55:57 +0100

    moved all the code to fluffy application class

--- app/src/fluffyApplication.cpp
+++ app/src/fluffyApplication.cpp
@@ -2,6 +2,20 @@
 
 #include <iostream>
 
-FluffyApplication::FluffyApplication() {
-    std::cout << "test" << std::endl;    
+FluffyApplication::FluffyApplication(int argc, char const *argv[]) {
+    output = std::make_shared<Output>();
+    auto flags = parseFlags(argc, argv);
+    handleFlags(flags);
+
+    auto databasePath{ std::filesystem::path(getExecutablePath().parent_path().string() + "/data.db")};
+    Database database{databasePath, output};
+    games = database.getGames();
+
+    if (argc - flags.size() > 1) {
+      auto [command, arguments] = parseCommand(argc, argv);
+      if (command != "") handleCommand(command, arguments, database);
+    }
+    else {
+      runConsoleApp();
+    }    
 }
